687a26a4378929b9c620b1c33e11c6a8ff7df4b0
Shelby Norris <shelbs9109@gmail.com>
2025-05-04 01:02:19 -0500

    Created html, js, css, and readme files; created array of objects, added headers to html, linked stylesheet and script to index file, created about and contact html files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # chore-tyrant
 A random chore generator for those who need a little motivation!
+
+## HTML
+## CSS / Bootstrap
+## JavaScript
+
---
+++ script.js
@@ -0,0 +1,53 @@
+console.log("Hello World")
+
+// Create an array of objects to display the chore item and the goofy "motivational" command for each
+
+
+// let chores = [
+//     {
+//         choreItem: "Wash the Dishes"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Start a Load of Laundry"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Fold Laundry"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Dust an Entire Room"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Vacuum an Entire Room"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Change your Bedding"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Wipe Down Kitchen Surfaces"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Deep Clean the Bathroom"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "De-clutter the Junk Drawer"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Organize Cabinets"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Sweep and Mop Flooring in One Room"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Sort Unwanted Clothes for Donations"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Clean Baseboards"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Wash Windows"
+//         choreCommand: ""
+//     }  {
+//         choreItem: "Take out the Trash"
+//         choreCommand: ""
+//     }
+// ]
