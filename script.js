2c0a66e5303cc027addc8ad2c346d6aa797c77fa
Shelby Norris <shelbs9109@gmail.com>
2025-05-05 11:51:50 -0500

    Completed array, added button and heading to html

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-console.log("Hello World")
+console.log("Hello World");
 
 // Create an array of objects to display the chore item and the goofy "motivational" command for each
 
+let chores = [
+    {
+        choreItem: "Wash the Dishes",
+        choreCommand: "You're one unwashed bowl away from a literal food swamp in your kitchen sink."
+    } , {
+        choreItem: "Start a Load of Laundry",
+        choreCommand: "Don't forget to dry them afterwards. No one needs mushrooms sprouting out of their pants."
+    } , {
+        choreItem: "Fold Laundry",
+        choreCommand: "I know good and well you see that mountain of unfolded mess. Get it done before two hobbits start climbing it with the One Ring."
+    } , {
+        choreItem: "Dust an Entire Room",
+        choreCommand: "No one wants furry lungs, bro. Vacuums and dusters are our friends."
+    } , {
+        choreItem: "Vacuum an Entire Room",
+        choreCommand: "Dirty floors suck. Know what else sucks? Vacuums. Handle that before you start sprouting carrots in the carpet."
+    } , {
+        choreItem: "Change your Bedding",
+        choreCommand: "The human body sheds about 600,000 skin cells per day. Now imagine how many have built up on those sheets of yours since you last changed them."
+    } , {
+        choreItem: "Wipe Down Kitchen Surfaces",
+        choreCommand: "The grease...the grime...the crumbs...just ew. Wipe e'm down."
+    } , {
+        choreItem: "Deep Clean the Bathroom",
+        choreCommand: "And when I say deep, I mean DEEP. I mean break out the chemicals and the elbow grease. Scrub like your life depends on it!"
+    } , {
+        choreItem: "De-clutter the Junk Drawer",
+        choreCommand: "You knew it had to happen one day. I mean, the drawer probably won't even fully close because it's so full of randomness. Time to purge!"
+    } , {
+        choreItem: "Organize Cabinets",
+        choreCommand: "Aren't you sick of getting pummeled in the face by an avalanche of tupperware or cough syrup bottles or whatever random items you may have in there every time you open the cabinet door?"
+    } , {
+        choreItem: "Sweep and Mop Flooring in One Room",
+        choreCommand: "No one wants to sound like they're Irish-dancing on Rice Krispies when they walk across a room, my guy. Handle that."
+    } , {
+        choreItem: "Sort Unwanted Clothes for Donations",
+        choreCommand: "They're taking up precious storage space, and they're probably not even that cute anyway."
+    } , {
+        choreItem: "Clean Baseboards",
+        choreCommand: "Yes, I'm serious. Run your fingertip across the top and you tell ME if you think they're 'fine'."
+    } , {
+        choreItem: "Wash Windows",
+        choreCommand: "The whole point of having a window is to, ya know, SEE outside. To VIEW the great outdoors. You can't do that if there's smudges all over the glass."
+    } , {
+        choreItem: "Take out the Trash",
+        choreCommand: "Look, you owe it to yourself to NOT have an actual bag of garbage sitting in your home. That's why we got rid of that ex of yours, remember?"
+    }
+]
 
-// let chores = [
-//     {
-//         choreItem: "Wash the Dishes"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Start a Load of Laundry"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Fold Laundry"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Dust an Entire Room"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Vacuum an Entire Room"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Change your Bedding"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Wipe Down Kitchen Surfaces"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Deep Clean the Bathroom"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "De-clutter the Junk Drawer"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Organize Cabinets"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Sweep and Mop Flooring in One Room"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Sort Unwanted Clothes for Donations"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Clean Baseboards"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Wash Windows"
-//         choreCommand: ""
-//     }  {
-//         choreItem: "Take out the Trash"
-//         choreCommand: ""
-//     }
-// ]
